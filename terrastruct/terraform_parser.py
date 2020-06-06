c4a8f65898f004e03add77173b7dbd6bfc0d26bb
Sergio Sanchez <sergio.sanchez0012@gmail.com>
2020-06-06 02:42:38 +0100

    Cleanup and parser handles comments

--- terrastruct/terraform_parser.py
+++ terrastruct/terraform_parser.py
@@ -25,7 +25,7 @@ def _line_parser(line):
 
 
 
-tf_file = open('./sample_data/sample_3.tf', 'r')
+tf_file = open('./sample_data/sample.tf', 'r')
 tf_lines = iter(tf_file.readlines())
 
 eof = False
@@ -49,57 +49,15 @@ while cur_line != None:
 			line_type, re_match = _line_parser(cur_line)
 			if line_type == "key_val":
 				tf_obj.add_arg(re_match.group("key"), re_match.group("val"))
+		tf_obj.disp()
 
+	if line_type == "start_comment":
+		while line_type != "end_comment":
+			cur_line = next(tf_lines)
+			line_type, re_match = _line_parser(cur_line)
 
-	tf_obj.disp()
 
 	cur_line = next(tf_lines, None)
 	line_type, re_match = _line_parser(cur_line)
-
-
-
-
-
-# x.strip('\"') for x in 
-
-
-
-# 	if in_comment
-
-# 	# not in comment
-
-# 	elif line_type == start_block:
-
-# 	elif line_type == end_block:
-
-
-# while not eof:
-# 	line_type = _line_parser(lines[line_num])
-# 	if line_type == "comment":
-
-# 	elif line_type == "start_comment":
-# 		while _line_parser(lines[line_num]) != "end_comment":
-# 			line_num++
-# 	elif line_type == start_block:
-
-# 	elif line_type == end_block:
-
-# 	line_num++
-
-
-
-
-
-	
-
-
-
-
-
-
-		
-
-
-
 
 
